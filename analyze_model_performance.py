9f41e4c9e8ce56ca33149e3d5f11251962a33ff3
clement <clement.tamines@gmail.com>
2019-11-01 00:08:56 +0100

    Added functions to extract samples hard to classify for a given model.

--- analyze_model_performance.py
+++ analyze_model_performance.py
@@ -76,4 +76,75 @@ def evaluate_model(model_path, classes, preprocessing, dataset_path):
 
     model = load_model(model_path)
     return model.evaluate_generator(validation_sample_generator, steps=math.ceil(nbr_val_samples / 16),
-                             max_queue_size=10, workers=1, use_multiprocessing=True, verbose=1)
+                                    max_queue_size=10, workers=1, use_multiprocessing=True, verbose=1)
+
+
+def extract_hard_samples(model_path, preprocess_input, dataset_path, threshold, image_size=(224, 224)):
+    """
+    Extracts samples which are ard to classify for the network. Takes a dataset and a model as input, prediction is
+    performed by the model on the samples from the dataset specified by dataset_path and samples with a classification
+    confidence for the correct class lower than threshold are saved to a list.
+    :param model_path:
+    :param preprocess_input:
+    :param dataset_path:
+    :param threshold:
+    :param image_size:
+    :return:
+    """
+
+    classes = ['fire', 'no_fire', 'start_fire']
+    nbr_classes = 3
+
+    model = load_model(model_path)
+
+    hard_examples = [[] for j in range(nbr_classes)]
+
+    for i in range(nbr_classes):
+
+        class_name = classes[i]
+
+        for sample_path in os.listdir(dataset_path + "/" + class_name):
+
+            img = image.load_img(dataset_path + "/" + class_name + "/" + sample_path, target_size=image_size)
+
+            # processed image to feed the network
+            processed_img = image.img_to_array(img)
+            processed_img = np.expand_dims(processed_img, axis=0)
+            processed_img = preprocess_input(processed_img)
+
+            # get prediction using the network
+            predictions = model.predict(processed_img)[0]
+
+            # if prediction is not satisfactory
+            if float(predictions[i]) < threshold:
+                hard_examples[i].append(sample_path)
+
+    return hard_examples
+
+
+def display_hard_samples(hard_examples, dataset_path):
+    """
+    Displays samples that are hard to classify from the dataset specified by dataset_path, hard_examples should be a
+    1x3 list containing paths of hard to classify samples.
+    :param hard_examples:
+    :param dataset_path:
+    :return:
+    """
+
+    classes = ['fire', 'no_fire', 'start_fire']
+
+    nbr_classes = 3
+
+    for i in range(nbr_classes):
+
+        class_name = classes[i]
+
+        print("========== CLASS : " + class_name + " ==========")
+        for sample_path in hard_examples[i]:
+            # load image for displaying
+            img = cv2.imread(dataset_path + "/" + class_name + "/" + sample_path)
+            # transform into RGB
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            plt.imshow(img)
+            plt.show()
